ba5e8c8fffe130e2eb9c22a40e9348f44e1c31fa
Minahil Muhammad Moiz <minahil4864@gmail.com>
2024-07-03 22:01:07 +0500

    updated button

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,8 +1,9 @@
 import ButtonSvg from "../assets/svg/ButtonSvg";
 
 const Button = ({ className, children, href, px, onClick, white }) => {
-  const classes =
-    'button relative infinite-flex items-center justify-center h-11 transition-colors hover:text-color-1 ${px || "px-7" } ${white? "text-n-8":"text-n-1} ${className || ""}';
+  const classes = `button relative inline-flex items-center justify-center h-11 transition-colors hover:text-color-1 ${
+    px || "px-7"
+  } ${white ? "text-n-8" : "text-n-1"} ${className || ""}`;
   const spanClasses = "relative z-10";
   const renderButton = () => (
     <button className={classes}>
