9c60e1f5ece2c4b6e9297cdfd4bd3f61f0d487af
Minahil Muhammad Moiz <minahil4864@gmail.com>
2024-07-08 12:04:47 +0500

    Completed Benefits Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import Button from "./Components/Button";
+import Benefits from "./Components/Benefits";
 import Header from "./Components/Header";
 import Hero from "./Components/Hero";
-import ButtonGradient from './assets/svg/ButtonGradient'
+import ButtonGradient from "./assets/svg/ButtonGradient";
 
 const App = () => {
   return (
     <>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
-        <Hero/>
+        <Hero />
+        <Benefits />
       </div>
-      <ButtonGradient/>
+      <ButtonGradient />
     </>
   );
 };
